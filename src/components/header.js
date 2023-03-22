ccd0835e3c3e27a216dfd444fc389d738564dce4
Sebastián González R <sebagonzalez10@outlook.es>
2023-03-22 09:21:40 -0400

    Agregando evento a los enlaces en para ocultar menú en la versión móvil

--- src/components/header.js
+++ src/components/header.js
@@ -12,11 +12,14 @@ const Header = () => {
 
 
     const showNav = (e) => {
-
-       console.log(navRef)
        navRef.current.classList.toggle("xs:hidden")
        navRef.current.classList.toggle("sm:hidden")
     }
+
+    const hiddenNav = (e) => {
+        navRef.current.classList.add("xs:hidden");
+        navRef.current.classList.add("sm:hidden");
+    } 
 
     return(
         <header className="w-screen p-4 bg-gray-900 header relative">
@@ -25,13 +28,13 @@ const Header = () => {
                 <nav className="flex justify-center items-center xs:hidden sm:hidden md:flex nav" ref={navRef}>
                     <ul className="xs:flex xs:flex-col xs:items-center xs:w-1/2 sm:flex sm:flex-col sm:items-center sm:w-1/2 md:flex md:justify-end md:flex-row">
                         <li>
-                            <Link className="mx-2 text-gray-500 font-semibold" href="/acerca">Acerca</Link>
+                            <Link onClick={hiddenNav} className="mx-2 text-gray-500 font-semibold" href="/acerca">Acerca</Link>
                         </li>
                         <li>
-                            <Link className="mx-2 text-gray-500 font-semibold" href="/proyectos">Proyectos</Link>
+                            <Link onClick={hiddenNav} className="mx-2 text-gray-500 font-semibold" href="/proyectos">Proyectos</Link>
                         </li>
                         <li>
-                            <Link className="mx-2 text-gray-500 font-semibold" href="/contacto">Contacto</Link>
+                            <Link onClick={hiddenNav} className="mx-2 text-gray-500 font-semibold" href="/contacto">Contacto</Link>
                         </li>
                     </ul>
                 </nav>
